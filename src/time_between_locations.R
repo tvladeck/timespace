b9f0eeb54c4af028736fb1aae440e6dc672c36d8
Thomas Vladeck <thomas.vladeck@gmail.com>
2015-08-11 13:31:02 -0400

    row 174 done

--- src/time_between_locations.R
+++ src/time_between_locations.R
@@ -1,8 +1,17 @@
 timeBetweenLocations <- function(row1, row2, data = addresses){
   address1 <- addresses[row1]
   address2 <- addresses[row2]
+  if (routeQueryCheck() > 0){
 
-  rt <- route(address1, address2, mode="transit")
-  time <- sum(rt$seconds)
-  return(time)
+  	tryCatch({
+  			rt <- route(address1, address2, mode="transit")
+  		}, error = function(e){
+  			rt <- route(address2, address1, mode="transit")
+  		}
+  	)
+
+  	time <- sum(rt$seconds)
+  	return(time)
+  }
+  
 }
